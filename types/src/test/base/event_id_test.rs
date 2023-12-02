68280e56107960fd1dc2c80e3a19b7ad197cd29e
Ruslan Valiev <ruslan.valiev@siemens.com>
2023-12-02 18:02:51 +0300

    Fix EventId

--- types/src/test/base/event_id_test.rs
+++ types/src/test/base/event_id_test.rs
@@ -1,26 +1,18 @@
-use time::OffsetDateTime;
+use derive_new::new;
 
-use crate::main::base::domain_event::EventId;
+use crate::main::base::domain_event::DomainEvent;
 
 #[allow(non_snake_case)]
 #[test]
 fn create_event__check_event_id_is_unique() {
     let firstEvent = EmptyEvent::new();
     let secondEvent = EmptyEvent::new();
-    assert_ne!(firstEvent.id, secondEvent.id);
-    assert_ne!(firstEvent.id.value, secondEvent.id.value)
+    assert_ne!(firstEvent.domain_events_params.id, secondEvent.domain_events_params.id);
+    assert_ne!(firstEvent.domain_events_params.id.value, secondEvent.domain_events_params.id.value)
 }
 
+#[derive(new)]
 struct EmptyEvent {
-    id: EventId,
-    _created: OffsetDateTime,
-}
-
-impl EmptyEvent {
-    pub fn new() -> Self {
-        Self {
-            id: EventId::new(),
-            _created: OffsetDateTime::now_utc(),
-        }
-    }
+    #[new(value = "DomainEvent::default()")]
+    domain_events_params: DomainEvent,
 }
